056395e114f8a3fb5003deab39d5ed44435624d6
jhem-jhem <jhempr15@gmail.com>
2025-06-15 13:59:13 +0800

    Activity 11: Email Notification, Done

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const bookRoutes = require('./routes/books');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/api/books', bookRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
